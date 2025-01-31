ce0f3d89ce4a4f23f1b7518324b1d3be30be4fc8
swaruprai10 <swaruprai10@gmail.com>
2025-02-01 00:05:47 +0545

    Add project files.

---
+++ MyApp/Data/MyAppContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.Models;
+
+namespace MyApp.Data
+{
+    public class MyAppContext : DbContext
+    {
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ItemClient>().HasKey(ic => new
+            {
+                ic.Itemid,
+                ic.ClientId
+            });
+            modelBuilder.Entity<ItemClient>().HasOne(i => i.Item).WithMany(ic => ic.ItemClients).HasForeignKey(i => i.Itemid);
+            modelBuilder.Entity<ItemClient>().HasOne(c => c.Client).WithMany(ic => ic.ItemClients).HasForeignKey(c => c.ClientId);
+            modelBuilder.Entity<Item>().HasData(
+                new Item { Id = 4, Name = "Microphone", Price = 3200, SerialNumberId = 10 }
+                );
+            modelBuilder.Entity<SerialNumber>().HasData(
+                new SerialNumber { Id = 10, Name = "MIC150", ItemId = 4 }
+                );
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Electronics" },
+                new Category { Id = 2, Name = "Books" },
+                new Category { Id = 3, Name = "Fruits" }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<SerialNumber> SerialNumbers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<ItemClient> ItemClients { get; set; }
+    }
+}
---
+++ MyApp/Migrations/20250131153828_one to one .cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class onetoone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "price",
+                table: "Items",
+                newName: "Price");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Items",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Items",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SerialNumberId",
+                table: "Items",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "SerialNumbers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SerialNumbers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SerialNumbers_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Items",
+                columns: new[] { "Id", "Name", "Price", "SerialNumberId" },
+                values: new object[] { 4, "Microphone", 3200.0, 10 });
+
+            migrationBuilder.InsertData(
+                table: "SerialNumbers",
+                columns: new[] { "Id", "ItemId", "Name" },
+                values: new object[] { 10, 4, "MIC150" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SerialNumbers_ItemId",
+                table: "SerialNumbers",
+                column: "ItemId",
+                unique: true,
+                filter: "[ItemId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SerialNumbers");
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DropColumn(
+                name: "SerialNumberId",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Items",
+                newName: "price");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Items",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Items",
+                newName: "id");
+        }
+    }
+}
